88b8e7ddbb3852d3b0ea635000e7f2a87c3cde4a
MightyMOuse <monique.diemert@gmail.com>
2022-10-04 16:36:15 -0400

    Launch reg

--- app/src/main/java/com/SEG2505_Group8/mealer/UI/Activities/MainActivity.java
+++ app/src/main/java/com/SEG2505_Group8/mealer/UI/Activities/MainActivity.java
@@ -2,6 +2,7 @@ package com.SEG2505_Group8.mealer.UI.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -79,6 +80,14 @@ public class MainActivity extends AppCompatActivity {
 
         register = findViewById(R.id.register);
         login = findViewById(R.id.login);
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: add new activity << chef or client>>
+            }
+        });
 
         mFirebaseAuth = FirebaseAuth.getInstance();
 
