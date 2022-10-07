8b6970ccd44a2937d8d3bc9599176d385aa142bc
Jeremy Cote <jeremymcote@gmail.com>
2022-10-07 08:53:23 -0400

    Fix go to homescreen logic

--- app/src/main/java/com/SEG2505_Group8/mealer/UI/Activities/MainActivity.java
+++ app/src/main/java/com/SEG2505_Group8/mealer/UI/Activities/MainActivity.java
@@ -94,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
         mFirebaseAuth = FirebaseAuth.getInstance();
 
         // checking if the user is null or not.
-        if (mFirebaseAuth.getCurrentUser() == null) {
+        if (mFirebaseAuth.getCurrentUser() != null) {
             tryLaunchHomeActivity();
         }
     }
