0d6dcd9ad3ab221ec3aa9afabf6253a0d9b64937
Jeremy <jeremymcote@gmail.com>
2022-10-12 21:08:43 -0400

    Prevent crash when email field is empty

--- app/src/main/java/com/SEG2505_Group8/mealer/UI/Activities/SignInActivity.java
+++ app/src/main/java/com/SEG2505_Group8/mealer/UI/Activities/SignInActivity.java
@@ -34,35 +34,7 @@ public class SignInActivity extends AppCompatActivity {
 
         emailContinueButton = findViewById(R.id.sign_in_continue_button);
         emailContinueButton.setOnClickListener(view -> {
-            // TODO: Check if email is already associated with an account
-
-            FirebaseAuth.getInstance().fetchSignInMethodsForEmail(email.getText().toString()).addOnCompleteListener(task -> {
-
-                boolean isNewUser = true;
-
-                try {
-                    isNewUser = task.getResult().getSignInMethods().isEmpty();
-                } catch (Exception e) {
-
-                }
-
-                if (isNewUser) {
-                    // Create the UserInfoForm intent
-                    Intent i = new Intent(SignInActivity.this, UserInfoForm.class);
-                    startActivity(i);
-
-                    // Kill our current intent
-                    finish();
-                } else {
-                    // Create the SignInWithEmail intent
-                    Intent i = new Intent(SignInActivity.this, SignInWithEmailActivity.class);
-                    i.putExtra("email", email.getText().toString());
-                    startActivity(i);
-
-                    // Kill our current intent
-                    finish();
-                }
-            });
+            redirectUser(email.getText().toString());
         });
 
         // If the user presses the back button on their phone
@@ -76,6 +48,38 @@ public class SignInActivity extends AppCompatActivity {
                 // Kill our current intent
                 finish();
             }
+        });
+    }
+
+    public void redirectUser(String email) {
+
+        if (email == null || email.isEmpty()) {
+            return;
+        }
+
+        FirebaseAuth.getInstance().fetchSignInMethodsForEmail(email).addOnCompleteListener(task -> {
+
+            boolean isNewUser = true;
+
+            try {
+                isNewUser = task.getResult().getSignInMethods().isEmpty();
+            } catch (Exception e) {
+                System.out.println("Failed to get SignInMethods. Presenting account creation.");
+            }
+
+            Intent i;
+            if (isNewUser) {
+                // Create the UserInfoForm intent
+                i = new Intent(SignInActivity.this, UserInfoForm.class);
+            } else {
+                // Create the SignInWithEmail intent
+                i = new Intent(SignInActivity.this, SignInWithEmailActivity.class);
+                i.putExtra("email", email);
+            }
+            startActivity(i);
+
+            // Kill current intent since we probably won't be back.
+            finish();
         });
     }
 }
