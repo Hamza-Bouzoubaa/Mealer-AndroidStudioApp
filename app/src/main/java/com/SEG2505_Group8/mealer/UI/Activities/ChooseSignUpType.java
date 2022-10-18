f01ba71c0df9607ef20b1ae47a976d83a075d458
MightyMOuse <monique.diemert@gmail.com>
2022-10-18 14:31:23 -0400

    not sure

---
+++ app/src/main/java/com/SEG2505_Group8/mealer/UI/Activities/ChooseSignUpType.java
@@ -0,0 +1,14 @@
+package com.SEG2505_Group8.mealer.UI.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChooseSignUpType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_sign_up_type);
+    }
+}
