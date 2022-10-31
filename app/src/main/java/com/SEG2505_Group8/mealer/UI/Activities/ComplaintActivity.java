d93ae8555b228f96b019b57616a4ee6e0c44975b
Jeremy Cote <jeremymcote@gmail.com>
2022-10-31 15:27:52 -0400

    Display date picker

--- app/src/main/java/com/SEG2505_Group8/mealer/UI/Activities/ComplaintActivity.java
+++ app/src/main/java/com/SEG2505_Group8/mealer/UI/Activities/ComplaintActivity.java
@@ -1,17 +1,21 @@
 package com.SEG2505_Group8.mealer.UI.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.SEG2505_Group8.mealer.Database.Models.MealerComplaint;
 import com.SEG2505_Group8.mealer.R;
+import com.SEG2505_Group8.mealer.UI.Fragments.DatePickerFragment;
 
 public class ComplaintActivity extends AppCompatActivity {
 
     MealerComplaint complaint;
     TextView complaintDescription;
+    Button suspendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +28,11 @@ public class ComplaintActivity extends AppCompatActivity {
         complaintDescription = findViewById(R.id.complaint_description);
 
         complaintDescription.setText(complaint.getDescription());
+
+        suspendButton = findViewById(R.id.complaint_date);
+        suspendButton.setOnClickListener(view -> {
+            DialogFragment newFragment = new DatePickerFragment();
+            newFragment.show(getSupportFragmentManager(), "datePicker");
+        });
     }
 }
