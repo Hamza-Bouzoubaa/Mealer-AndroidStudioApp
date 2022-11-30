4eebfea88990750b30dd6a62986f562d749d128e
Adam Jami <adamjam39@gmail.com>
2022-11-30 00:31:20 -0500

    <zzz>

---
+++ app/src/main/java/com/SEG2505_Group8/mealer/UI/Activities/ProfilView.java
@@ -0,0 +1,27 @@
+package com.SEG2505_Group8.mealer.UI.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.SEG2505_Group8.mealer.R;
+import com.SEG2505_Group8.mealer.Services;
+
+public class ProfilView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_view);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Services.getDatabaseClient().getUser(user -> {
+            user.getId();
+
+        });
+    }
+}
